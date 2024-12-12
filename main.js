31b9c79847edbda115d05fe1260684a5abcfeacb
Ben <61493854+bengera@users.noreply.github.com>
2024-12-12 20:35:30 +0700

    add basic JS

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-console.log('Testing line for JavaScript🚗🚗');
+'use strict'
+const taskField = document.getElementById('add-task');
 
-// const taskItems = document.querySelectorAll('.tasks__left-block-content');
+class Task {
+      
+    static printMessage(message){
+        console.log(`You added ${message}`)
 
+    }
+
+   
+    static handleInput(taskField, e){
+        if  (e.key === 'Enter' && taskField.value.trim() !== ''){
+            Task.printMessage(taskField.value);
+            taskField.value = ''
+        }
+    }
+
+    
+}
+
+taskField.addEventListener('keydown', (e) => Task.handleInput(taskField, e))
+
+
+
+// Can add to separate ManagerClass using method
+// Can use a static method in Task class
+// taskField.addEventListener('keydown', (e) => {
+//     if(e.key === 'Enter'){
+//         console.log('Entered');
+//         console.log(taskField.value)
+
+//     }
+// })
