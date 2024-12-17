5fa4552193046cc903669e6cb1c1cdff130581cd
Ben <61493854+bengera@users.noreply.github.com>
2024-12-17 15:10:45 +0700

    add tasks to array

--- main.js
+++ main.js
@@ -1,34 +1,69 @@
-'use strict'
-const taskField = document.getElementById('add-task');
+"use strict";
+const taskField = document.getElementById("task-field"); // input field
 
+let listArr = [];
+
+// event listener
+taskField.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault(); // without this, attempt made to submit form
+    let id = Date.now() + Math.floor(Math.random());
+    const newTask = new Task(id, taskField.value.trim())
+    listArr = [...listArr, newTask] // expand array and add newTask
+    taskField.value = "";
+    console.log(listArr);
+  }
+});
+
+
+// make object instance
 class Task {
-      
-    static printMessage(message){
-        console.log(`You added ${message}`)
-
-    }
-
-   
-    static handleInput(taskField, e){
-        if  (e.key === 'Enter' && taskField.value.trim() !== ''){
-            Task.printMessage(taskField.value);
-            taskField.value = ''
-        }
-    }
-
-    
+  constructor(id, task){
+    this.id = id;
+    this.task = task;
+  }
 }
 
-taskField.addEventListener('keydown', (e) => Task.handleInput(taskField, e))
+class UI {
+
+}
 
 
 
-// Can add to separate ManagerClass using method
-// Can use a static method in Task class
-// taskField.addEventListener('keydown', (e) => {
-//     if(e.key === 'Enter'){
-//         console.log('Entered');
-//         console.log(taskField.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Task { // no constructor to initialize?
+
+//     static printMessage(message){
+//         console.log(`You added ${message}`)
 
 //     }
-// })
+
+//     // static handleInput(taskField, e){
+//     //     if  (e.key === 'Enter' && taskField.value.trim() !== ''){
+//     //         Task.printMessage(taskField.value);
+//     //         taskField.value = ''
+//     //     }
+//     // }
+
+// }
+
+// // taskField.addEventListener('keydown', (e) => Task.handleInput(taskField, e))
+
+// Task.greeting = function(){
+//     console.log('Hello everyone star')
+// }
