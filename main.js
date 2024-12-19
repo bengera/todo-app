b677607b6a1e01306f7b8300d532806c8196d854
Ben <61493854+bengera@users.noreply.github.com>
2024-12-19 16:16:04 +0700

    add storage saving

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const list = document.querySelector(".tasks__list");
 
 let listArr = [];
 
+// By using static classes we can call them methods without having an instance of class
 taskField.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
@@ -11,11 +12,14 @@ taskField.addEventListener("keydown", (e) => {
   }
 });
 
+
 // make object instance
 class Task {
+  
   constructor(id, description) {
     this.id = id;
     this.description = description;
+          
     // taskField.addEventListener("keydown", this._handleTask.bind(this))
   }
 
@@ -48,59 +52,31 @@ class Task {
       })
       .join("");
 
+      Task.setLocalStorage()
     list.innerHTML += allTasks;
     console.log(listArr);
+    
+  }
+
+  static setLocalStorage(){
+    localStorage.setItem('allTasks',JSON.stringify(listArr));
+    console.log('data stored')
+  }
+
+  static getLocalStorage(){
+    const data = JSON.parse(localStorage.getItem('allTasks'))
+    if (data){
+      listArr = [...data]; // merge data into listArr
+      console.log(`${data.length} tasks retrieved`)
+    }
+    
+    
   }
 }
 
-// for each array items
-// class UI {
-//   static addTasks() {
-//     list.innerHTML = ''; // creates issue when refresh, lose checkbox
-//     let allTasks = listArr.map(task =>{
-//       return `
-//       <div class="tasks__item-block">
-//                   <div class="tasks__inner-item-block">
-//                     <div class="tasks__left-block-content">
-//                       <input type="checkbox" id="${task.id}" class="tasks__checkbox">
-//                       <label class="tasks__text truncate" for="${task.id}">
-//                         ${task.description}
-//                       </label>
-//                     </div>
-//                     <button class="tasks__btn-delete"></button>
-//                   </div>
-//                 </div>
-//       `
-//     }).join('')
 
-//     list.innerHTML += allTasks;
-//     console.log(listArr);
+document.addEventListener('DOMContentLoaded',()=> {
+  Task.getLocalStorage();
+  Task.addTasks();
+})
 
-//   }
-
-// }
-
-//Bugs
-// each input item must be give a unique id in order to work
-
-// class Task { // no constructor to initialize?
-
-//     static printMessage(message){
-//         console.log(`You added ${message}`)
-
-//     }
-
-//     // static handleInput(taskField, e){
-//     //     if  (e.key === 'Enter' && taskField.value.trim() !== ''){
-//     //         Task.printMessage(taskField.value);
-//     //         taskField.value = ''
-//     //     }
-//     // }
-
-// }
-
-// // taskField.addEventListener('keydown', (e) => Task.handleInput(taskField, e))
-
-// Task.greeting = function(){
-//     console.log('Hello everyone star')
-// }
