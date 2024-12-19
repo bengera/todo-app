dc6d80c2e9c8a2216fabcd234f044f8f68ebaae4
Ben <61493854+bengera@users.noreply.github.com>
2024-12-19 12:20:56 +0700

    add handleTask

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 "use strict";
-const taskField = document.getElementById("task-field"); 
-const list = document.querySelector('.tasks__list');
+const taskField = document.getElementById("task-field");
+const list = document.querySelector(".tasks__list");
 
 let listArr = [];
 
-// event listener
 taskField.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    e.preventDefault(); // without this, attempt made to submit form
-    let id = Date.now() + Math.floor(Math.random());
-    const newTask = new Task(id, taskField.value.trim())
-    // listArr = [...listArr, newTask] // expand array and add newTask
-    listArr.push(newTask)
-    taskField.value = "";
-    UI.addTasks();
+    e.preventDefault();
+    Task.handleTask();
   }
 });
 
-
 // make object instance
 class Task {
-  constructor(id, description){
+  constructor(id, description) {
     this.id = id;
     this.description = description;
+    // taskField.addEventListener("keydown", this._handleTask.bind(this))
   }
 
-  
-}
+  static handleTask() {
+    console.log("handling new task");
+    let id = Date.now() + Math.floor(Math.random());
+    const newTask = new Task(id, taskField.value.trim());
+    listArr.push(newTask);
+    taskField.value = "";
+    Task.addTasks();
+  }
 
-// for each array items
-class UI {
   static addTasks() {
-    list.innerHTML = ''; // creates issue when refresh, lose checkbox
-    let allTasks = listArr.map(task =>{
-      return `
+    list.innerHTML = ""; // creates issue when refresh, lose checkbox
+    let allTasks = listArr
+      .map((task) => {
+        return `
       <div class="tasks__item-block">
                   <div class="tasks__inner-item-block">
                     <div class="tasks__left-block-content">
@@ -45,37 +44,44 @@ class UI {
                     <button class="tasks__btn-delete"></button>
                   </div>
                 </div>
-      `
-    }).join('')
+      `;
+      })
+      .join("");
 
     list.innerHTML += allTasks;
     console.log(listArr);
-    
-  }
-
-  static checkArr(){
-
   }
 }
 
+// for each array items
+// class UI {
+//   static addTasks() {
+//     list.innerHTML = ''; // creates issue when refresh, lose checkbox
+//     let allTasks = listArr.map(task =>{
+//       return `
+//       <div class="tasks__item-block">
+//                   <div class="tasks__inner-item-block">
+//                     <div class="tasks__left-block-content">
+//                       <input type="checkbox" id="${task.id}" class="tasks__checkbox">
+//                       <label class="tasks__text truncate" for="${task.id}">
+//                         ${task.description}
+//                       </label>
+//                     </div>
+//                     <button class="tasks__btn-delete"></button>
+//                   </div>
+//                 </div>
+//       `
+//     }).join('')
 
+//     list.innerHTML += allTasks;
+//     console.log(listArr);
+
+//   }
+
+// }
 
 //Bugs
 // each input item must be give a unique id in order to work
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // class Task { // no constructor to initialize?
 
