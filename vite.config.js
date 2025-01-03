48a7e9bb8d65311d513d84be4929536659a75597
Ben <61493854+bengera@users.noreply.github.com>
2025-01-03 17:19:06 +0700

    change vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import purgecss from 'vite-plugin-purgecss';
+import purgecss from 'vite-plugin-purgecss-updated-v5';
 import { resolve } from 'path';
 
 export default defineConfig({
